9cfa8b77c364769c2a3a69166c200bff7a221853
ocblanchette <ocblanchette@wpi.edu>
2020-04-06 13:49:32 -0400

    firebase database

--- app/src/main/java/com/example/afgcharity/MainActivity.java
+++ app/src/main/java/com/example/afgcharity/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
+                // This method iscalled once with the initial value and again
                 // whenever data at this location is updated.
                 String value = dataSnapshot.getValue(String.class);
                 Log.d("tag", "Value is: " + value);
